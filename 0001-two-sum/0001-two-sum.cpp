9b4e15ecb3b27bea787292ea8ac864995583dbce
Ankita S <ankitasatish0711@gmail.com>
2026-09-13 17:12:20 +0530

    Refactor to implement two-sum function

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,37 +1,18 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode dummy; // dummy head
-        ListNode* curr = &dummy;
-        int carry = 0;
+vector<int> twoSum(vector<int>& nums, int target) {
 
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int sum = carry;
-            if (l1 != nullptr) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if (l2 != nullptr) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
+unordered_map<int, int> mp;
 
-            carry = sum / 10;
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
-        }
+for (int i= 0; i < nums.size(); i++) {
 
-        return dummy.next;
-    }
-};
+int complement = target - nums[i];
+
+// Check if complement already exists
+if (mp.find(complement) != mp.end()) {
+return {mp[complement], i};
+
+// Store number and its index
+mp[nums [i]] = i;
+
+return {;
